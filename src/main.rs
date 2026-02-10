7eacab2f227b21c69582c5f9350d87f60275ee1b
agrovate <nishantullal@icloud.com>
2026-02-10 11:42:52 +0530

    adding functions start,stop and show

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env::{self, current_dir};
-use sqlx::{Sqlite, SqlitePool, migrate::MigrateDatabase, FromRow};
+use sqlx::{FromRow, Row, Sqlite, SqlitePool, migrate::MigrateDatabase};
 use dotenvy::{dotenv, from_path};
 use chrono::{DateTime, Duration, Local};
 
@@ -9,10 +9,8 @@ pub struct Project {
    pub dir: String,
    pub start: DateTime<Local>,
    pub stop: Option<DateTime<Local>>,
-   pub duration: i64 
+   pub duration: Option<i64>
 }
-
-
 
 #[tokio::main]
 async fn main() {
@@ -31,27 +29,61 @@ async fn main() {
         }
     } 
     let db = SqlitePool::connect(&db_url).await.unwrap();
-    
+    let action = "show";
+    match action {
+       "start" => start(&db).await.unwrap(),
+       "stop" => stop(&db).await.unwrap(),
+       "show" => show(&db).await.unwrap(),
+        _ => println!("shi is broken")
+    }
+}
 
-
-    // To get current directory name
+async fn start(db: &SqlitePool) -> Result<(), sqlx::Error> {
     let loc = current_dir().unwrap().to_string_lossy().to_string();
     let dirs:Vec<&str> = loc.split("/").collect();
-    let starts = Local::now();
-    let stops =  starts + Duration::days(1) ;
-    let p1 = Project {
+    let start_time = Local::now();
+
+    let p = Project {
          dir : dirs[dirs.len() - 1].to_string(),
-         start: starts,
-         stop: Some(stops),
-         duration: (stops - starts).num_seconds()
+         start: start_time,
+         stop: None,
+         duration: None
     };
 
-    sqlx::query!("insert into times(dir, start, duration) values($1, $2, $3)",
-        p1.dir, p1.start, p1.duration
-    ).execute(&db).await.unwrap();
-    let data = sqlx::query_as::<_, Project>("select * from times;").fetch_all(&db).await.unwrap();
-    
-    for row in data {
-        println!("{:?}", row);
-    }
+    sqlx::query!("insert into times(dir, start) values($1, $2)",
+        p.dir, p.start
+    ).execute(db).await.unwrap();
+
+    Ok(())
+}
+
+
+async fn stop(db: &SqlitePool) -> Result<(), sqlx::Error> {
+    let loc = current_dir().unwrap().to_string_lossy().to_string();
+    let dirs:Vec<&str> = loc.split("/").collect();
+    let stop_time = Local::now();
+    let row = sqlx::query("select start from times where dir = $1")
+        .bind(dirs[dirs.len() - 1].to_string())
+        .fetch_one(db)
+        .await?;
+
+    let start_time: DateTime<Local> = row.try_get("start")?;
+
+    let diff = stop_time - start_time; 
+    sqlx::query("UPDATE times SET stop = $1, duration = $2 WHERE dir = $3")
+        .bind(stop_time)
+        .bind(diff.num_seconds())
+        .bind(dirs[dirs.len() - 1].to_string())
+        .execute(db)
+        .await?;
+
+    Ok(())
+}
+
+async fn show(db: &SqlitePool) -> Result<(), sqlx::Error> {
+    let data = sqlx::query_as::<_,Project>("select * from times")
+        .fetch_all(db).await?;
+    println!("{:?}",data);
+
+    Ok(())
 }
